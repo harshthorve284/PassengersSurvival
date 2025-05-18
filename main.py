d2a6f38d3979e766cfea262c80aa237045033001
harshthorve284 <harshthoreve284@gmail.com>
2025-05-18 22:37:42 +0530

    done

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as sl
+sl.title("Hi!")
+sl.header("Yo")
+sl.subheader('My name is harsh')
+
+json={"a":"1,2,3","b":"4,5,6"}
+sl.json(json)
+code="""
+print("Hellow World)
+def func():
+    return 0;"""
+
